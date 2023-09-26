4f2c73325cbce82d28481bab317e8eaeb420ff61
puze zhong <zpz9991@gmail.com>
2023-09-26 15:06:09 -0500

    Build client

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3003;
 
 app.use(express.static(path.join(__dirname, "../client/build")));
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
