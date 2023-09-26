3ce15add335ec7d8c665352a2dc1c05a5c7e36a7
puze zhong <zpz9991@gmail.com>
2023-09-26 15:50:10 -0500

    s

--- server/server.js
+++ server/server.js
@@ -8,9 +8,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV) {
-  app.use(express.static("../client/build"));
-  app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
