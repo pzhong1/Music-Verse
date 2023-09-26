00475b465685cb9608b72326ae1ade97adb3486c
puze zhong <zpz9991@gmail.com>
2023-09-26 14:33:27 -0500

    deploy on heroku

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3003;
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
 });
 
 app.listen(PORT, () => {
