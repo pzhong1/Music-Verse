ff5080791d5fa1ea35f16c66688fac9947b3709e
puze zhong <zpz9991@gmail.com>
2023-09-26 16:57:52 -0500

    Fix Heroku

--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
-const express = require("express");
+// server.js
 const path = require("path");
+const express = require("express");
 const app = express();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
