a8f2987b772dd0f6129071457e7c3136cf33e224
puze zhong <zpz9991@gmail.com>
2023-09-26 15:42:22 -0500

    pls work

--- server/server.js
+++ server/server.js
@@ -7,13 +7,12 @@ const PORT = process.env.PORT || 3003;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+if (process.env.NODE_ENV) {
+  app.use(express.static("app/client/build"));
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "app/client/build", "index.html"));
+  });
 }
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
