8b62b2822faa199f71266ed95699da445fadce41
puze zhong <zpz9991@gmail.com>
2023-09-26 18:22:45 -0500

    pls work

--- server/server.js
+++ server/server.js
@@ -1,6 +1,24 @@
-const path = require("path");
 const express = require("express");
+const spotifyService = require("./utils/spotifyService");
+const cors = require("cors");
+const path = require("path");
+
 const app = express();
+
+app.use(cors({ origin: "http://localhost:3000" }));
+
+app.use(express.json()); // To handle JSON requests
+app.use(express.static(path.join(__dirname, "public"))); // Serve static files
+
+app.get("/search", async (req, res) => {
+  try {
+    const query = req.query.q;
+    const results = await spotifyService.searchMusic(query);
+    res.json(results);
+  } catch (error) {
+    res.status(500).send("Server error");
+  }
+});
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
@@ -8,7 +26,8 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
