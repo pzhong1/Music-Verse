31d05b3bc570fb817939f19f3571a91f5cf7f102
puze zhong <zpz9991@gmail.com>
2023-09-26 15:11:13 -0500

    up

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
 const PORT = process.env.PORT || 3003;
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
 
 app.get("*", (req, res) => {
   const indexPath = path.join(__dirname, "../client/build/index.html");
