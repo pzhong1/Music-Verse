274c85b7459a4216d84a9b371f91f33d725fadba
puze zhong <zpz9991@gmail.com>
2023-09-26 14:19:14 -0500

    delpoy heroku

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const connectDB = require("./config/connection");
+const path = require("path");
 
 const app = express();
 connectDB();
 
 const PORT = process.env.PORT || 3003;
 
-// routes
-app.get("/", (req, res) => {
-  res.send("Server is running");
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(PORT, () => {
