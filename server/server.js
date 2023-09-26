47e6fb974d224d318c013fb6d58a0b5bbeeca3bf
puze zhong <zpz9991@gmail.com>
2023-09-26 16:52:49 -0500

    Fix server static file path

--- server/server.js
+++ server/server.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 3003;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static(path.join(__dirname, "../client/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
