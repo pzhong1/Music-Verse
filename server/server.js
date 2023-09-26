88df0557d8e316af2f305c587a4ab5c2b6d857c4
puze zhong <zpz9991@gmail.com>
2023-09-26 15:55:38 -0500

    s

--- server/server.js
+++ server/server.js
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 3003;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV) {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
   });
 }
 
