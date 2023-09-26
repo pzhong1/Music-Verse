7c49938d5bbeb58c5709d7228eb58441191feb66
puze zhong <zpz9991@gmail.com>
2023-09-26 17:27:03 -0500

    Re-add client directory

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomePage from "./components/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
