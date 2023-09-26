b49a828618440c116054871c7fd4632c68ab7209
puze zhong <zpz9991@gmail.com>
2023-09-26 16:08:05 -0500

    s

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 app.listen(PORT, () => {
