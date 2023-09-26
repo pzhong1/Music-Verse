2800ee47f8641312a06a81f2893ee84ea0d31d8c
puze zhong <zpz9991@gmail.com>
2023-09-26 14:58:36 -0500

    pls work

--- server/server.js
+++ server/server.js
@@ -3,10 +3,9 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3003;
 
-app.use(express.static(path.join(__dirname, "../client/build")));
-
+app.use(express.static("client/build"));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  res.sendFile(path.resolve("client", "build", "index.html"));
 });
 
 app.listen(PORT, () => {
