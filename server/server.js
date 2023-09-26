7c69a766c08fb6d94481840b90b630faca50e77b
puze zhong <zpz9991@gmail.com>
2023-09-26 17:44:26 -0500

    s

--- server/server.js
+++ server/server.js
@@ -2,13 +2,11 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-const root = path.join(__dirname, "../");
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(root, "client", "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(root, "client", "build", "index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
 
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
