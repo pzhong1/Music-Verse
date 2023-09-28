3fa08b37532b4bb428fdeb5005bed7ab3c79983f
JacobDFlores <jacobfdaniel@gmail.com>
2023-09-27 21:02:58 -0500

    Super hot freaking fix

--- server/server.js
+++ server/server.js
@@ -4,15 +4,31 @@ const spotifyService = require("./utils/spotifyService");
 const path = require("path");
 const { ApolloServer } = require('apollo-server-express');
 const cors = require('cors'); // Import the cors middleware
-const app = express();
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas');
+
+
 const PORT = process.env.PORT || 3001;
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json()); // To handle JSON requests
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 // Enable CORS for all routes
 app.use(cors());
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); // To handle JSON requests
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
 
 app.get("/search", async (req, res) => {
   try {
@@ -23,25 +39,6 @@ app.get("/search", async (req, res) => {
     res.status(500).send("Server error");
   }
 });
-
-const { typeDefs, resolvers } = require('./schemas');
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
-
-
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});
-
-
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
