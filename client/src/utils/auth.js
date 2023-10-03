3b73ef5bc9c5db74702030b16af609596042c698
puze zhong <zpz9991@gmail.com>
2023-10-02 19:25:53 -0500

    fixing heroku issue

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,24 +1,23 @@
-import decode from 'jwt-decode';
+import decode from "jwt-decode";
 
 class AuthService {
   getProfile() {
     let decoded = null; // Define decoded here with a default value
-  
+
     try {
       const token = this.getToken();
-      console.log('Token:', token); // Add this line for debugging
+      console.log("Token:", token); // Add this line for debugging
       if (!token) return null;
-  
+
       decoded = decode(token); // Assign a value to decoded
-      console.log('Decoded Token:', decoded); // Add this line for debugging
+      console.log("Decoded Token:", decoded); // Add this line for debugging
     } catch (error) {
-      console.error('Error decoding token:', error.message);
+      console.error("Error decoding token:", error.message);
       return null;
     }
-  
+
     return decoded; // Return the decoded token
   }
-  
 
   isAuthenticated() {
     const token = this.getToken();
@@ -31,15 +30,15 @@ class AuthService {
       if (decodedToken.exp && decodedToken.exp < currentTime) {
         // Token has expired
         // Handle expiration, such as logging the user out
-        console.log('Token has expired');
+        console.log("Token has expired");
         return false;
       } else {
         // Token is valid
-        console.log('Token is valid');
+        console.log("Token is valid");
         return true;
       }
     } catch (error) {
-      console.error('Error decoding token:', error.message);
+      console.error("Error decoding token:", error.message);
       return false;
     }
   }
@@ -52,24 +51,24 @@ class AuthService {
   isTokenExpired(token) {
     const decoded = decode(token);
     if (decoded.exp < Date.now() / 1000) {
-      localStorage.removeItem('id_token');
+      localStorage.removeItem("id_token");
       return true;
     }
     return false;
   }
 
   getToken() {
-    return localStorage.getItem('id_token');
+    return localStorage.getItem("id_token");
   }
 
   login(idToken) {
-    localStorage.setItem('id_token', idToken);
-    window.location.assign('/');
+    localStorage.setItem("id_token", idToken);
+    window.location.assign("/");
   }
 
   logout() {
-    localStorage.removeItem('id_token');
-    window.location.reload();
+    localStorage.removeItem("id_token");
+    window.location.assign("/");
   }
 }
 
